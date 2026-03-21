8a05453d18505d76c25626eb200d395bbbd3cfe8
Admin <Admin@DESKTOP-A04AQ48>
2026-03-21 13:17:48 +0200

    Add project files.

---
+++ tests/BlockiumLauncher.Application.Tests/SmokeTests.cs
@@ -0,0 +1,10 @@
+﻿namespace BlockiumLauncher.Application.Tests;
+
+public class SmokeTests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/BlockiumLauncher.Infrastructure.Tests/SmokeTests.cs
@@ -0,0 +1,10 @@
+﻿namespace BlockiumLauncher.Infrastructure.Tests;
+
+public class SmokeTests
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
